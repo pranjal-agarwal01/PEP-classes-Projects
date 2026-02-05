a80ef01e254a5b416aafaf7df7bb3c758474366e
humble-guyy <agarwalpranjal2006@gmail.com>
2026-02-05 10:41:07 +0530

    react starting

---
+++ day7/my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  const name ="Pranjal";
+  return (
+    <>
+    <h1> Hello Everyone</h1>
+    <div className ="box"></div>
+    onClick={handleClick}
+    <h2>Hello {name}</h2>
+    </>
+  )
+}
+export default App
