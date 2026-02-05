8bcaa53bdb284908ff941b4771a1f7fade2055a2
humble-guyy <agarwalpranjal2006@gmail.com>
2026-02-05 11:37:27 +0530

    today done

--- day7/my-react-app/src/App.jsx
+++ day7/my-react-app/src/App.jsx
@@ -1,16 +1,16 @@
-import './App.css'
+// import './App.css'
 
-function Header() {
- return <h2> This is outside function</h2>
-}
+// function Header() {
+//  return <h2> This is outside function</h2>
+// }
 
-function App() {
-  return (
-    <div>
-      <Header/>
-      <p> this is inside the function?</p>
-    </div>
-  )
-}
+// function App() {
+//   return (
+//     <div>
+//       <Header/>
+//       <p> this is inside the function?</p>
+//     </div>
+//   )
+// }
 
-export default App
+// export default App
