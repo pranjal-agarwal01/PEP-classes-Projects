5d4dcf0e9b2fd28444c49cbf87178147864580ab
humble-guyy <agarwalpranjal2006@gmail.com>
2026-02-06 16:20:28 +0530

    today final

--- day7/my-react-app/src/App.jsx
+++ day7/my-react-app/src/App.jsx
@@ -1,77 +1,135 @@
+// // import './App.css'
+
+// // function Header() {
+// //  return <h2> This is outside function</h2>
+// // }
+
+// // function App() {
+// //   return (
+// //     <div>
+// //       <Header/>
+// //       <p> this is inside the function?</p>
+// //     </div>
+// //   )
+// // }
+
+// // export default App
+
 // import './App.css'
 
-// function Header() {
-//  return <h2> This is outside function</h2>
-// }
-
-// function App() {
-//   return (
-//     <div>
-//       <Header/>
-//       <p> this is inside the function?</p>
-//     </div>
-//   )
-// }
-
-// export default App
-
-import './App.css'
-
-function ProfileCard (){
-    const greeting = "Hello!";
-    const age = 25;
-    const city = "New York";
+// // function ProfileCard (){
+// //     const greeting = "Hello!";
+// //     const age = 25;
+// //     const city = "New York";
     
 
-    return (
-        <div className="card">
-            <h1> {greeting}</h1>
-            <p>Age: {age}</p>
-            <p>City: {city}</p>
-            <img src = "/MY photo on platforms.png" alt = "Profile Picture"/>
+// //     return (
+// //         <div className="card">
+// //             <h1> {greeting}</h1>
+// //             <p>Age: {age}</p>
+// //             <p>City: {city}</p>
+// //             <img src = "/MY photo on platforms.png" alt = "Profile Picture"/>
 
-        </div>
+// //         </div>
 
-    )
-}
-function callName(name){
-    alert(name);
-}
+// //     )
+// // }
+// // function callName(name){
+// //     alert(name);
+// // }
 
-import {useState} from 'react';
-function IncrementCounter(){
-    const[count, setCount] = useState (0);
-    return (
+//   import {useState} from 'react';
 
-        <div>
-            <h1> Count: {count}</h1>
-            { (count ===0 || count === 10) && (
-                <p> Count is either 0 or 10!</p>
-            )}
-            <button onClick ={() => setCount(count+1)} disabled = {count===10}> Increment</button>
-            <button onClick = { () => setCount(count-1) } disabled ={count===0}> Decrement</button>
-            <button onClick = { () => setCount(0) }  > Reset </button> 
+//   const [user, setUser] = useState({
+//     name : "Pranjal",
+//     age : 20,
+//     City: "Bareilly"
+//   })
+
+//   setUser(prev => ({
+//     ...prev,
+//     name : "Agarwal"
+//   }));
+
+//   const[stats,setStats] = useState({\
+//     likes:10,
+//     comments:2
+//   })
+//   setStats(prev => ({
+//     ...prev,
+//     likes: prev.likes+1
+//   }))
+
+
+
+
+// // function IncrementCounter(){
+// //     const[count, setCount] = useState (0);
+// //     return (
+
+// //         <div>
+// //             <h1> Count: {count}</h1>
+// //             { (count ===0 || count === 10) && (
+// //                 <p> Count is either 0 or 10!</p>
+// //             )}
+// //             <button onClick ={() => setCount(count+1)} disabled = {count===10}> Increment</button>
+// //             <button onClick = { () => setCount(count-1) } disabled ={count===0}> Decrement</button>
+// //             <button onClick = { () => setCount(0) }  > Reset </button> 
             
              
             
+// //         </div>
+// //     )
+
+
+
+// // }
+
+// function App() {
+//     // function callName(name){
+//     //     alert(name);
+    
+//     return (
+//         // <div>
+//         //     <IncrementCounter/>
+//         // </div>
+//         // // <div>
+//         //     <button onClick={() => callName("Sneha")} > Sneha</button>
+//         // </div>
+//         // <div className="App">
+//         //     <ProfileCard/>
+//         // </div>
+//     )
+// }
+// export default App;
+
+import './App.css'
+import { useState } from 'react'
+
+function App(){
+    const [user, setUser] = useState ({
+        name : "Pranjal",
+        age: 20,
+        city: "Bareilly"
+    });
+
+    function changeName(){
+        setUser(prev => ({
+            ...prev,
+            name: "Agarwal"
+        }))
+    }
+
+
+    return (
+        <div style = {{padding : "20px" , fontFamily: "Arial"}}> 
+            <h2> User Profile</h2>
+            <p>Name: {user.name}</p>
+            <p>Age: {user.age}</p>
+            <p>City: {user.city}</p>
+            <button onClick={changeName}>Change Name</button>
         </div>
     )
 }
 
-function App() {
-    // function callName(name){
-    //     alert(name);
-    
-    return (
-        <div>
-            <IncrementCounter/>
-        </div>
-        // <div>
-        //     <button onClick={() => callName("Sneha")} > Sneha</button>
-        // </div>
-        // <div className="App">
-        //     <ProfileCard/>
-        // </div>
-    )
-}
 export default App;
